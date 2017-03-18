01acc27f54b5eb0174460cc6e173e7fc3a649dc8
kanlijun <kanlijun@mobanker.com>
2017-03-18 19:21:33 +0800

    finish toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
-  validates :content, length: {maximum: 140}
+  validates :content, length: {maximum: 140},
+            presence: true   #存在性验证，不可为空
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 #encoding:UTF-8
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
